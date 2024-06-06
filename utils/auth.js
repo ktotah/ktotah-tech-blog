bcce305b8315d1d4d6665ce3e4fcf47a1898c8d5
ktotah <ket2137@columbia.edu>
2024-06-06 16:01:12 -0700

    updated formatting in utils/auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,10 @@
 // Middleware to check if the user is authenticated
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
-  
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
