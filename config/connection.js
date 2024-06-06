6247dad5440b5642a17a1bf692ba761ec18908dd
ktotah <ket2137@columbia.edu>
2024-06-06 02:54:34 -0700

    updated comments

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Create a connection to our database, pass in your MySQL information for username and password
+// Create a connection the database, pass in the MySQL information for username and password
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
