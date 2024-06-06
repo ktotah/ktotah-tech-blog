aa9b9590fc92adc91f874a353b9d2d56fe8fa98f
ktotah <ket2137@columbia.edu>
2024-06-06 15:26:17 -0700

    added js to handle comment form submission, login form submission, signup form submission, and new post creation and deletion

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+// Handle comment form submission
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('#comment').value.trim();
+    const post_id = window.location.toString().split('/').pop();
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ comment_text, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+  
