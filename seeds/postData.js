fceb33c7c03ddeab0768ea98ec0beeccbde69c02
ktotah <ket2137@columbia.edu>
2024-06-07 23:45:33 -0700

    updated styling

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,17 +3,17 @@ const { Post } = require('../models');
 const postData = (users) => [
   {
     title: 'Why Learning Potions is Important',
-    content: 'Potions allow wizards and witches to maintain a true mastery of magical substances...',
+    content: 'Potions allow wizards and witches to maintain a true mastery of magical substances, enabling them to create powerful elixirs and brews that can heal, enhance abilities, or even cause harm.',
     user_id: users.find(user => user.username === 'HPotter').id,
   },
   {
     title: 'Differences Between Hexes and Curses',
-    content: 'There is a difference between hexes and curses. Hexes are minor dark charms...',
+    content: 'There is a difference between hexes and curses. Hexes are minor dark charms intended to inconvenience the target, while curses are more powerful and intended to cause significant harm or lasting damage.',
     user_id: users.find(user => user.username === 'HGranger').id,
   },
   {
     title: 'Apparition Tips and Tricks',
-    content: 'I have really loved learning about apparition. It has really simplified travel...',
+    content: 'I have really loved learning about apparition. It has really simplified travel, making it possible to instantly move from one location to another with ease, though it requires concentration and practice to master.',
     user_id: users.find(user => user.username === 'RWeasley').id,
   },
   {
