f94e9eefa919b78f6b5304fea502af119a879af3
ktotah <ket2137@columbia.edu>
2024-06-08 20:48:55 -0400

    Update database configuration to use JawsDB MySQL

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
-// Import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Create a connection the database, pass in the MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
